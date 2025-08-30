283f6cf40850ec3e6fb3c308c1678f3ad06fde49
alkobr-55 <o.ghneem@mediable.io>
2025-08-30 17:44:14 +0300

    frontend-setup

---
+++ reactapp/tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: "class", // enable class-based dark mode
+};
