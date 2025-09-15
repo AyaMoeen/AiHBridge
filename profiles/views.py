f2b7fc72159721c1b69e8b6d45b54eee184bdf3c
RaghadDeraweah <s11924798@stu.najah.edu>
2025-09-15 12:56:45 +0300

    add trendy & highlighted tools feature

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,15 @@
+import json
 from rest_framework import viewsets, permissions, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
+
+from posts.models import Category, Post
+from posts.serializers import PostSerializer
+from django.db.models import Count, Avg
+
+
 
 class ProfileViewSet(viewsets.ViewSet):
     """
@@ -11,7 +18,8 @@ class ProfileViewSet(viewsets.ViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        return Profile.objects.select_related('user')
+        return Profile.objects.select_related('user').prefetch_related('interested_categories')
+       # return Profile.objects.select_related('user')
 
     def list(self, request):
         """
@@ -31,3 +39,20 @@ class ProfileViewSet(viewsets.ViewSet):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    @action(detail=False, methods=['get'], url_path="highlighted-tools")
+    def highlighted_tools(self, request):
+        profile = request.user.profile
+
+        posts = Post.objects.filter(
+            categories__in=profile.interested_categories.all()
+        ).annotate(
+            likes_count=Count('reactions', distinct=True),
+            comments_count=Count('comments', distinct=True),
+            avg_rating=Avg('ratings__value')
+        ).distinct().order_by(
+            '-avg_rating'
+        )
+        #'-likes_count', '-comments_count', 
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
