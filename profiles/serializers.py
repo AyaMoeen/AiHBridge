167d6c338b776a95af047d6e137769c72bf9e0b8
RaghadDeraweah <s11924798@stu.najah.edu>
2025-09-16 12:16:33 +0300

    updated profiles (serializers, views)

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,15 +1,17 @@
 from rest_framework import serializers
 from .models import Profile
-from posts.models import Category
+from posts.models import Category, Post
+from posts.serializers import PostSerializer
+from follows.models import Follow
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     user_id = serializers.IntegerField(source="user.id", read_only=True)
     email = serializers.EmailField(source="user.email", read_only=True)
-    username=serializers.CharField(source="user.username", read_only=True)
-    name=serializers.CharField(source="user.name", read_only=True)
+    username = serializers.CharField(source="user.username", read_only=True)
+    name = serializers.CharField(source="user.name", read_only=True)
     created_at = serializers.DateTimeField(source='user.date_joined', read_only=True)
-    
+
     interested_category_ids = serializers.PrimaryKeyRelatedField(
         queryset=Category.objects.all(),
         many=True,
@@ -17,10 +19,22 @@ class ProfileSerializer(serializers.ModelSerializer):
         write_only=True,
         required=False
     )
-    
     interested_categories = serializers.StringRelatedField(many=True, read_only=True)
+
+    followers_count = serializers.SerializerMethodField()
+    following_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Profile
-        fields = ["id", "user_id", "email", "username", "name", "created_at", "bio", "profile_picture",
-                  "interested_categories", "interested_category_ids"]
+        fields = [
+            "id", "user_id", "email", "username", "name", "created_at",
+            "bio", "profile_picture",
+            "interested_categories", "interested_category_ids",
+            "followers_count", "following_count",
+        ]
+
+    def get_followers_count(self, obj):
+        return Follow.objects.filter(following=obj.user).count()
+
+    def get_following_count(self, obj):
+        return Follow.objects.filter(follower=obj.user).count()
